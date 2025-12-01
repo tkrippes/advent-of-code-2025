801a1ab51f2309cdf312acb82e172de728fe50ac
Tom Krippes <tkrippes@live.de>
2025-12-01 15:11:20 +0100

    Add solution for part 2 of day 1 (Python)
    add instructions for part 2
    add results from part 2

--- day_01/python/main.py
+++ day_01/python/main.py
@@ -2,7 +2,7 @@ import os
 
 from .rotation import Rotation
 from .parser.rotations_parser import parse_rotations
-from .solver.password_solver import solve_password
+from .solver.password_solver import solve_password_part_1, solve_password_part_2
 
 
 def part_1() -> int:
@@ -16,8 +16,23 @@ def part_1() -> int:
     )
     rotations: list[Rotation] = parse_rotations(input_file_name)
 
-    return solve_password(rotations)
+    return solve_password_part_1(rotations)
+
+
+def part_2() -> int:
+    """
+    Solve part 2 of day 1."""
+    input_file_name: str = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "input",
+        "input.txt",
+    )
+    rotations: list[Rotation] = parse_rotations(input_file_name)
+
+    return solve_password_part_2(rotations)
 
 
 if __name__ == "__main__":
     print(f"Result of part 1: {part_1()}")
+    print(f"Result of part 2: {part_2()}")
--- day_01/python/solver/password_solver.py
+++ day_01/python/solver/password_solver.py
@@ -1,11 +1,11 @@
 from ..rotation import Rotation, RotationDirection
 
 
-def solve_password(rotations: list[Rotation]) -> int:
+def solve_password_part_1(rotations: list[Rotation]) -> int:
     """
     Solve password based on the list of rotations and dial start position 50.
 
-    The password is determinec by the number of time the dial is pointing
+    The password is determined by the number of time the dial is pointing
     to 0 after any rotation.
     """
     dial_start_position: int = 50
@@ -20,10 +20,54 @@ def solve_password(rotations: list[Rotation]) -> int:
         else:
             raise ValueError(f"Unhandled rotation direction: {rotation.direction}")
 
-        # set dial position in range 0-99
+        # reset dial position in range 0-99
         current_dial_position %= 100
         if current_dial_position == 0:
             password += 1
+
+    return password
+
+
+def solve_password_part_2(rotations: list[Rotation]) -> int:
+    """
+    Solve password based on the list of rotations and dial start position 50.
+
+    The password is determined by the number of time the dial is pointing
+    to 0 at any time.
+    """
+    min_dial_position: int = 0
+    max_dial_position: int = 99
+    dial_start_position: int = 50
+    current_dial_position: int = dial_start_position
+    last_dial_position: int = dial_start_position
+    password: int = 0
+
+    for rotation in rotations:
+        last_dial_position = current_dial_position
+
+        if rotation.direction == RotationDirection.LEFT:
+            current_dial_position -= rotation.distance
+
+            # add one for each time the dial crosses 0
+            while current_dial_position < min_dial_position:
+                current_dial_position += max_dial_position + 1
+                password += 1
+
+            # add one if dial points exactly on 0
+            if current_dial_position == min_dial_position:
+                password += 1
+
+            # substract one if dial started on 0 (no crossing)
+            if last_dial_position == min_dial_position:
+                password -= 1
+        elif rotation.direction == RotationDirection.RIGHT:
+            current_dial_position += rotation.distance
+
+            while current_dial_position > max_dial_position:
+                current_dial_position -= max_dial_position + 1
+                password += 1
+        else:
+            raise ValueError(f"Unhandled rotation direction: {rotation.direction}")
 
     return password
 
@@ -43,4 +87,9 @@ if __name__ == "__main__":
         Rotation(RotationDirection.LEFT, 82),
     ]
 
-    print(f"Password for test rotations: {solve_password(test_rotations)}")
+    print(
+        f"Password for test rotations (part 1): {solve_password_part_1(test_rotations)}"
+    )
+    print(
+        f"Password for test rotations (part 2): {solve_password_part_2(test_rotations)}"
+    )
